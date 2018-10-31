daeb00b4347ffdbd1a19f3d4fde6eb48b3e87a18
victor <vpereira648@gmail.com>
2018-10-30 22:05:17 -0300

    primeira aula de js

---
+++ script.js
@@ -0,0 +1,23 @@
+// let nome = 'Victor Hugo';
+// let idade = 23;
+// let vivo = false;
+
+// let saida1 = 'Olá, ' + '! Tudo bem com voce?';
+// saida1 = `Olá, ${nome}! voce tem ${idade} anos!`;
+
+// console.log('saida1');
+// // alert('Hello world');
+// // console.log('passous do alert');
+
+// console.log(prompt('Qual seu nome'));
+// idade = (prompt('quantos anos voce tem?'));
+// console.log (`voce tem ${idade}`);
+// vivo = confirm('voce esta vivo');
+// console.log(`voce esta vivo`${vivo});
+
+let valor1 = Number (prompt('digite um valor'));
+let valor2 = Number (prompt('digite outro valor'));
+
+console.log(typeof(valor1));
+alert(valor1 + valor2);
+
